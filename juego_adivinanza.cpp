92593fe6050a7c6e65a9d843b0df769dbc0c1f85
fabrine44re-eng <fabeine44.re@gmail.com>
2026-03-20 13:20:27 -0400

    2026-03-20_01 programa de adivinanza en c++

---
+++ juego_adivinanza.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"************************************"<<endl;
+cout<<"Bienvenido al juego de la adivinanza"<<endl;
+cout<<"************************************"<<endl;
+
+}
